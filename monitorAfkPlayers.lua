1613bc278ed383d455b5dde3c5698280d7c51bf1
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2024-05-16 00:02:24 +0200

    Update monitorAfkPlayers.lua

--- monitorAfkPlayers.lua
+++ monitorAfkPlayers.lua
@@ -10,7 +10,7 @@ function pluralize(word, count)
     end
 end
 
-local CURRENT_SCRIPT_VERSION <const> = "0.3"
+local CURRENT_SCRIPT_VERSION <const> = "0.4"
 local TITLE <const> = "Monitor AFK players v" .. CURRENT_SCRIPT_VERSION
 
 local MY_ROOT <const> = menu.my_root()
@@ -25,23 +25,38 @@ local logInToast = false
 local logInConsole = false
 local includeDeathEvents = true
 local includeRagdollEvents = true
+local ignoreInteriors = true
 local is_any_afk_found_in_iteration = false
 
-local function isMenuRefValid(pedPid)
+local function isMenuAfkPlayerRefValid(pedPid)
+    if not playerList[pedPid] then
+        return false
+    end
+
     return menu.is_ref_valid(menu.ref_by_rel_path(MY_ROOT, playerList[pedPid].name))
 end
 
-local function updateAfkPlayerHelpText(pedPid, totalAfkTime)
-    menu.set_help_text(menu.ref_by_rel_path(MY_ROOT, playerList[pedPid].name), "Last Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovementTime) ..  " | Total " .. pluralize("second", totalAfkTime) ..  " AFK: " .. totalAfkTime)
+local function deleteMenuAfkPlayerRef(pedPid)
+    menu.delete(menu.ref_by_rel_path(MY_ROOT, playerList[pedPid].name))
 end
 
-local function logAfkPlayerDetection(pedPid, totalAfkTime, hideNotifications)
+local function updateMenuAfkPlayerRefHelpText(pedPid, pedPos, totalAfkTime)
+    menu.set_help_text(menu.ref_by_rel_path(MY_ROOT, playerList[pedPid].name),
+        "Last Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovement_time) .. " | Total " .. pluralize("second", totalAfkTime) .. " AFK: " .. totalAfkTime
+        .. "\n\nPlayer Coordinates:\nX: " .. pedPos.x .. "\nY: " .. pedPos.y .. "\nZ: " .. pedPos.z
+    )
+end
+
+local function logAfkPlayerDetection(pedPid, pedPos, totalAfkTime, hideNotifications)
     hideNotifications = hideNotifications or false
 
-    if isMenuRefValid(pedPid) then
-        updateAfkPlayerHelpText(pedPid, totalAfkTime)
+    if isMenuAfkPlayerRefValid(pedPid) then
+        updateMenuAfkPlayerRefHelpText(pedPid, pedPos, totalAfkTime)
     else
-        MY_ROOT:list(playerList[pedPid].name, {}, "Last Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovementTime) ..  " | Total " .. pluralize("second", totalAfkTime) ..  " AFK: " .. totalAfkTime, function()
+        MY_ROOT:list(playerList[pedPid].name, {},
+            "Last Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovement_time) .. " | Total " .. pluralize("second", totalAfkTime) .. " AFK: " .. totalAfkTime
+            .. "\n\nPlayer Coordinates:\nX: " .. pedPos.x .. "\nY: " .. pedPos.y .. "\nZ: " .. pedPos.z
+        , function()
             menu.player_root(pedPid):trigger()
         end)
     end
@@ -54,25 +69,27 @@ local function logAfkPlayerDetection(pedPid, totalAfkTime, hideNotifications)
         util.toast(
             "[Lua Script]: " .. TITLE .. "\n"
             .. "\nPlayer " .. playerList[pedPid].name .. " is detected AFK!\n"
-            .. "\nLast Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovementTime)
+            .. "\nLast Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovement_time)
             .. "\nTotal Time AFK: " .. totalAfkTime .. pluralize(" second", totalAfkTime)
         )
     end
 
     if logInConsole then
         local paddedPlayerName = string.format("%-16s", playerList[pedPid].name)
-        print("[Lua Script]: " .. TITLE .. " | Player " .. paddedPlayerName .. " is detected AFK! | Last Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovementTime) ..  " | Total " .. pluralize("second", totalAfkTime) ..  " AFK: " .. totalAfkTime)
+        print("[Lua Script]: " .. TITLE .. " | Player " .. paddedPlayerName .. " is detected AFK! | Last Movement: " .. os.date("%H:%M:%S", playerList[pedPid].lastMovement_time) ..  " | Total " .. pluralize("second", totalAfkTime) ..  " AFK: " .. totalAfkTime)
         util.yield()
     end
 end
 
 MY_ROOT:toggle_loop("Monitor AFK Players", {}, "Checks if the player don't move for a given ammount of time.", function()
     util.yield() -- No need to spam it.
-    print(PLAYER.IS_PLAYER_PLAYING(players.user()))
 
-    if not util.is_session_started() then
-        playerList = {}
-    end
+    players.on_leave(function(pedPid)
+        if isMenuAfkPlayerRefValid(pedPid) then
+            deleteMenuAfkPlayerRef(pedPid)
+        end
+        playerList[pedPid] = nil
+    end)
 
     if is_any_afk_found_in_iteration then
         is_any_afk_found_in_iteration = false
@@ -86,6 +103,28 @@ MY_ROOT:toggle_loop("Monitor AFK Players", {}, "Checks if the player don't move 
     end
 
     for players.list() as pedPid do
+        if
+            not NETWORK.NETWORK_IS_PLAYER_CONNECTED(pedPid)
+            or not NETWORK.NETWORK_IS_PLAYER_ACTIVE(pedPid)
+            or NETWORK.NETWORK_IS_PLAYER_FADING(pedPid)
+            or NETWORK.IS_PLAYER_IN_CUTSCENE(pedPid)
+            or NETWORK.NETWORK_IS_PLAYER_IN_MP_CUTSCENE(pedPid)
+        then
+            goto CONTINUE
+        end
+
+        if
+            ignoreInteriors
+            and players.is_in_interior(pedPid)
+        then
+            if isMenuAfkPlayerRefValid(pedPid) then
+                deleteMenuAfkPlayerRef(pedPid)
+            end
+            playerList[pedPid] = nil
+            goto CONTINUE
+        end
+
+
         local pPed = PLAYER.GET_PLAYER_PED(pedPid)
         if pPed == 0 then
             goto CONTINUE
@@ -96,18 +135,18 @@ MY_ROOT:toggle_loop("Monitor AFK Players", {}, "Checks if the player don't move 
         if not playerList[pedPid] then
             playerList[pedPid] = {
                 name = players.get_name(pedPid),
-                lastMovementTime = os.time(),
+                lastMovement_time = os.time(),
                 isDead = false,
-                hasRespawnedFromDeath = false,
+                hasRespawnedFromDeath_time = nil,
                 isInRagdoll = false,
-                hasStandUpFromRagdoll = false,
+                hasStandUpFromRagdoll_time = nil,
                 x = pedPos.x,
                 y = pedPos.y,
                 z = pedPos.z
             }
 
             if everyoneAfkAtLaunch then
-                logAfkPlayerDetection(pedPid, 0, true)
+                logAfkPlayerDetection(pedPid, pedPos, 0, true)
             end
 
             goto CONTINUE
@@ -116,16 +155,21 @@ MY_ROOT:toggle_loop("Monitor AFK Players", {}, "Checks if the player don't move 
         if includeDeathEvents then
             if PLAYER.IS_PLAYER_DEAD(pedPid) then
                 playerList[pedPid].isDead = true
-                playerList[pedPid].hasRespawnedFromDeath = false
+                playerList[pedPid].hasRespawnedFromDeath_time = os.time()
             elseif playerList[pedPid].isDead then
-                playerList[pedPid].isDead = false
-                playerList[pedPid].hasRespawnedFromDeath = PLAYER.IS_PLAYER_PLAYING(pedPid)
-            elseif playerList[pedPid].hasRespawnedFromDeath then
-                playerList[pedPid].hasRespawnedFromDeath = false
+                if
+                    PLAYER.IS_PLAYER_PLAYING(pedPid)
+                    and (os.time() - playerList[pedPid].hasRespawnedFromDeath_time) >= 3 -- Allows an extra margin of safety, considering the game's usual 0.5-second adjustment for player positions.
+                then
+                    playerList[pedPid].isDead = false
+                    playerList[pedPid].hasRespawnedFromDeath_time = nil
+                end
+            elseif playerList[pedPid].hasRespawnedFromDeath_time then
+                playerList[pedPid].hasRespawnedFromDeath_time = nil
             end
         else
             playerList[pedPid].isDead = false
-            playerList[pedPid].hasRespawnedFromDeath = false
+            playerList[pedPid].hasRespawnedFromDeath_time = nil
         end
 
         if includeRagdollEvents then
@@ -134,30 +178,39 @@ MY_ROOT:toggle_loop("Monitor AFK Players", {}, "Checks if the player don't move 
                 or PED.IS_PED_RAGDOLL(pPed)
             then
                 playerList[pedPid].isInRagdoll = true
+                playerList[pedPid].hasStandUpFromRagdoll_time = os.clock()
             elseif playerList[pedPid].isInRagdoll then
-                playerList[pedPid].isInRagdoll = false
+                if (os.clock() - playerList[pedPid].hasStandUpFromRagdoll_time) >= 3 then -- Allows an extra margin of safety, considering the game's usual 0.3-second adjustment for player positions.
+                    playerList[pedPid].isInRagdoll = false
+                    playerList[pedPid].hasStandUpFromRagdoll_time = nil
+                end
+            elseif playerList[pedPid].hasStandUpFromRagdoll_time then
+                playerList[pedPid].hasStandUpFromRagdoll_time = nil
             end
         else
             playerList[pedPid].isInRagdoll = false
+            playerList[pedPid].hasStandUpFromRagdoll_time = nil
         end
 
         if
             playerList[pedPid].isDead
-            or playerList[pedPid].hasRespawnedFromDeath
+            or playerList[pedPid].hasRespawnedFromDeath_time
             or playerList[pedPid].isInRagdoll
+            or playerList[pedPid].hasStandUpFromRagdoll_time
         then
             playerList[pedPid].x = pedPos.x
             playerList[pedPid].y = pedPos.y
             playerList[pedPid].z = pedPos.z
+
         elseif
             pedPos.x ~= playerList[pedPid].x
             or pedPos.y ~= playerList[pedPid].y
             or pedPos.z ~= playerList[pedPid].z
         then
-            playerList[pedPid].lastMovementTime = os.time()
+            playerList[pedPid].lastMovement_time = os.time()
 
-            if menu.is_ref_valid(menu.ref_by_rel_path(MY_ROOT, playerList[pedPid].name)) then
-                menu.delete(menu.ref_by_rel_path(MY_ROOT, playerList[pedPid].name))
+            if isMenuAfkPlayerRefValid(pedPid) then
+                deleteMenuAfkPlayerRef(pedPid)
             end
 
             playerList[pedPid].x = pedPos.x
@@ -167,12 +220,12 @@ MY_ROOT:toggle_loop("Monitor AFK Players", {}, "Checks if the player don't move 
             goto CONTINUE
         end
 
-        local totalAfkTime = os.time() - playerList[pedPid].lastMovementTime
+        local totalAfkTime = os.time() - playerList[pedPid].lastMovement_time
         if totalAfkTime >= afkTimer then
             is_any_afk_found_in_iteration = true
-            logAfkPlayerDetection(pedPid, totalAfkTime)
-        elseif isMenuRefValid(pedPid) then
-            updateAfkPlayerHelpText(pedPid, totalAfkTime)
+            logAfkPlayerDetection(pedPid, pedPos, totalAfkTime)
+        elseif isMenuAfkPlayerRefValid(pedPid) then
+            updateMenuAfkPlayerRefHelpText(pedPid, pedPos, totalAfkTime)
         end
 
         :: CONTINUE ::
@@ -191,12 +244,15 @@ end)
 OPTIONS:toggle("Everyone AFK at launch", {}, "When enabled, sets all players as AFK when the script starts.", function(toggle)
     everyoneAfkAtLaunch = toggle
 end, everyoneAfkAtLaunch)
-OPTIONS:toggle("Include death events", {}, "When enabled, AFK detection continues even if a player's character dies.", function(toggle)
+OPTIONS:toggle("Include Death Events", {}, "When enabled, AFK detection continues even if a player's character dies.", function(toggle)
     includeDeathEvents = toggle
 end, includeDeathEvents)
-OPTIONS:toggle("Include ragdoll events", {}, "When enabled, AFK detection continues even if a player's character receives a ragdoll effect.", function(toggle)
+OPTIONS:toggle("Include Ragdoll Events", {}, "When enabled, AFK detection continues even if a player's character receives a ragdoll effect.", function(toggle)
     includeRagdollEvents = toggle
 end, includeRagdollEvents)
+OPTIONS:toggle("Ignore Interiors", {}, "When enabled, ignores AFK detection for players inside interiors.", function(toggle)
+    ignoreInteriors = toggle
+end, ignoreInteriors)
 OPTIONS:divider("---------------------------------------")
 OPTIONS:divider("Logging Options:")
 OPTIONS:divider("---------------------------------------")
